f94adf955b6cc6d5769656b176b933065ac6255b
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-01 14:09:37 -0500

    Completed data types exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,40 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "My name is Darien, what is yours?";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//
+//        System.out.println(x);
+//        System.out.println(y);
+
+        int x = Integer.MAX_VALUE;
+        System.out.println(x);
     }
 }
