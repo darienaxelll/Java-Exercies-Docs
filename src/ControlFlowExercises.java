849ae048296d33b8be0bd6d17aba9006839c3085
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-02 15:22:52 -0500

    completed table exercise and grade exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        int x = 5;
@@ -20,8 +22,57 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
-        for (long i = 2; i < 1000000; i*=i) {
-            System.out.println(i);
-        }
+//        for (long i = 2; i < 1000000; i*=i) {
+//            System.out.println(i);
+//        }
+
+//
+//        for (int i = 1; i <= 100; i++) {
+////            boolean equalToThree = (i % 3 == 0);
+////            boolean equalToFive = (i % 5 == 0);
+//
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("What number would you like to go up to? ");
+//        int userInput = scanner.nextInt();
+//
+//        System.out.println("Here is your table!");
+//        System.out.println();
+//        System.out.printf("number | squared | cubed%n" +
+//                         "------ | ------ | ------ %n");
+//
+//        for (int i = 1; i <= userInput; i++) {
+//            int number = i;
+//            int squared = i * i;
+//            int cubed = i * i * i;
+//
+//            System.out.printf("   %s   |   %s   |   %s   %n", number, squared, cubed);
+//        }
+
+//        System.out.println("What grade did you get?");
+//        int grade = scanner.nextInt();
+//
+//        if (grade <= 100 && grade > 88) {
+//            System.out.println("You got an A");
+//        } else if (grade < 87 && grade > 80) {
+//            System.out.println("You got a B");
+//        } else if (grade < 79 && grade >67) {
+//            System.out.println("You got a C");
+//        } else if (grade < 66 && grade > 60) {
+//            System.out.println("You got a D");
+//        } else {
+//            System.out.println("You got a D");
+//        }
     }
 }
