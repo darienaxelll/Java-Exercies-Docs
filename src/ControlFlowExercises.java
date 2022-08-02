53841620cd07fe365ba13a7fae4ac0c2c7218dcc
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-02 11:02:39 -0500

    completed exercise 2 on ControlFlowExercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,20 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int x = 5;
+//        int x = 5;
+//
+//        while (x <= 15) {
+//            System.out.println(x);
+//            x++;
+//        }
 
-        while (x <= 15) {
-            System.out.println(x);
-            x++;
-        }
+        int i = 0;
+
+        do {
+
+            i += 2;
+
+            System.out.println(i);
+
+        } while (i < 100);
     }
 }
