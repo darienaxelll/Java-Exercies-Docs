fc1824394f68331c06c2b8c8351fc15d4697b358
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-03 09:30:42 -0500

    Completed StringExercise

---
+++ src/StringExercise.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class StringExercise {
+    public static void main(String[] args) {
+
+        String[] oddLines = {"We don't need no education\n" + "We don't need no thought control", "Check \"this\" out!, \"s inside of \"s!", "In windows, the main drive is usually C:\\", "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!"};
+
+        System.out.println(Arrays.toString(oddLines));
+    }
+}
