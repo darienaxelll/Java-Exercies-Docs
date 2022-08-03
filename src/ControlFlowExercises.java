8d1e447747399306a5f60728ad77fb1e2d52a870
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-03 14:46:06 -0500

    completes facorial exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -74,5 +74,6 @@ public class ControlFlowExercises {
 //        } else {
 //            System.out.println("You got a D");
 //        }
+
     }
 }
---
+++ src/MethodsExercises.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter a number between 1 and 10: ");
+//        int userInput = scanner.nextInt();
+//
+//        getInteger(userInput, 1, 10);
+//        factorial(userInput);
+
+    }
+
+
+//    public static int Addition(int n1, int n2) {
+//        return n1 + n2;
+//    }
+//
+//    public static int Subtraction(int n1, int n2) {
+//        return n1 - n2;
+//    }
+//
+//    public static int Multiplication(int n1, int n2) {
+//        return n1 * n2;
+//    }
+//
+//    public static int Division(int n1, int n2) {
+//        if (n2 == 0) {
+//            System.out.println("Error");
+//        }
+//        return n1 / n2;
+//    }
+//
+//    public static int Modulus(int n1, int n2) {
+//        return n1 % n2;
+//    }
+
+//    public static void getInteger(int userInput, int min, int max) {
+//        if (userInput > min && userInput < max) {
+//            System.out.println("Number is in range");
+//        } else {
+//            System.out.println("Number not in range");
+//        }
+//    }
+
+//    public static void factorial(int userInput) {
+//
+//        if (userInput >= 1 && userInput <= 10) {
+//            long i,fact=1;
+//            for(i=1;i<=userInput;i++){
+//                fact=fact*i;
+//            }
+//            System.out.println("Factorial of "+userInput+" is: "+fact);
+//        } else {
+//            System.out.println("Invalid number");
+//        }
+//    }
+
+    public static void diceRolling() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number of sides for a pair of dice...");
+        int userInput = scanner.nextInt();
+
+
+    }
+}
+
