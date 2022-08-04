7836dfe518d7945cf9ef5866872ecf7d63452bb5
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-04 10:05:37 -0500

    Working on high/low

--- src/HighLow.java
+++ src/HighLow.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 public class HighLow {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int upperbound = 1 + random.nextInt(100);
+        System.out.println(upperbound);
+
         System.out.println("Guess a number between 1 and 100: ");
         int userInput = scanner.nextInt();
 
+        do {
 
+            if (userInput == upperbound) {
+                System.out.println("You are right!");
+            } else if (userInput > upperbound) {
+                System.out.println("Guess is wrong, you went to high");
+            } else if (userInput < upperbound) {
+                System.out.println("Guess is wrong, you went to low");
+            }
+        } while ();
     }
-
-    public static void randomNumber(int userInput) {
-        Random random  = new Random();
-        int upperbound = 101;
-
-
-    }
-
-
-
 }
