d0025fd2d8c9fe336acc9d967ef3b53d4a12527c
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-04 10:29:37 -0500

    completed high/low game

--- src/HighLow.java
+++ src/HighLow.java
@@ -3,24 +3,42 @@ import java.util.Scanner;
 
 public class HighLow {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         int upperbound = 1 + random.nextInt(100);
         System.out.println(upperbound);
 
-        System.out.println("Guess a number between 1 and 100: ");
-        int userInput = scanner.nextInt();
+//        while  (true) {
+//            Scanner scanner = new Scanner(System.in);
+//
+//            System.out.println("Guess a number between 1 and 100: ");
+//            int userInput = scanner.nextInt();
+//
+//            if (userInput > upperbound) {
+//                System.out.println("Too high");
+//            } else if (userInput < upperbound) {
+//                System.out.println("Too low");
+//            } else if (userInput == upperbound) {
+//                System.out.println("right");
+//                break;
+//            }
+//        }
 
         do {
 
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Guess a number between 1 and 100: ");
+            int userInput = scanner.nextInt();
+
             if (userInput == upperbound) {
                 System.out.println("You are right!");
+                break;
             } else if (userInput > upperbound) {
                 System.out.println("Guess is wrong, you went to high");
             } else if (userInput < upperbound) {
                 System.out.println("Guess is wrong, you went to low");
             }
-        } while ();
+        } while (true);
     }
 }
