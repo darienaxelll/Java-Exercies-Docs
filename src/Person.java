6dd1c39fc9b24d9e90aad2373bcfb63840402b35
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-05 11:19:49 -0500

    Completed Person exercise

---
+++ src/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    private String name;
+
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println("Hello, " + getName());
+    }
+
+    public static void main(String[] args) {
+        Person Jason = new Person();
+
+        Jason.setName("Jason");
+        Jason.sayHello();
+
+    }
+}
