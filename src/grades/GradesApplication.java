208a3c283737a2bb824b8909b70879c778b47c8f
Darien Salazar <darien.axell.salazar@gmail.com>
2022-08-15 15:40:58 -0500

    Completed grades app

---
+++ src/grades/GradesApplication.java
@@ -0,0 +1,66 @@
+package grades;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import util.Input;
+
+
+
+public class GradesApplication {
+    public static void main(String[] args) {
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
+        Input input = new Input();
+
+        Student student1 = new Student("Josh".toLowerCase(), "TheBoiJosh");
+        student1.addGrade(90);
+        student1.addGrade(75);
+        student1.addGrade(100);
+
+        Student student2 = new Student("Nick", "NickDaKick");
+        student2.addGrade(85);
+        student2.addGrade(100);
+        student2.addGrade(78);
+
+        Student student3 = new Student("Cathryn", "ShyCat");
+        student3.addGrade(80);
+        student3.addGrade(88);
+        student3.addGrade(100);
+
+        Student student4 = new Student("Jacob", "White_beard");
+        student4.addGrade(90);
+        student4.addGrade(75);
+        student4.addGrade(80);
+
+
+        HashMap<String, Student> students = new HashMap<>();
+        students.put("Josh", student1);
+        students.put("Nick", student2);
+        students.put("Cathryn", student3);
+        students.put("Jacob", student4);
+
+        System.out.println("Welcome!");
+        System.out.println();
+        System.out.println("Here are the GitHub usernames of our students: ");
+        System.out.println(students.keySet());
+        System.out.println();
+
+        while (true) {
+            System.out.println("What student would you like to see more information on: ");
+            String userInput = input.getString();
+            System.out.println();
+
+            if (students.containsKey(userInput)) {
+                System.out.println(students.get(userInput).getName() + " - GitHub Username: " + students.get(userInput).getUsername());
+                System.out.println("Current Average: " + numberFormat.format(students.get(userInput).getGradeAverage()));
+                System.out.println();
+            }
+            else if (userInput.equalsIgnoreCase("no")) {
+                System.out.println("Goodbye, and have a wonderful day!");
+                break;
+            } else {
+                    System.out.println("Sorry, no student found with the GitHub username of " + userInput);
+                    System.out.println();
+                }
+            }
+        }
+    }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,29 +1,36 @@
 package grades;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Student {
     private final String name;
-    private int grade;
+    private final ArrayList<Integer> gradeList = new ArrayList<>();
+    private String username;
 
-    ArrayList<Integer> gradeList = new ArrayList<>();
-
-    public Student(String name, int grade) {
+    public Student(String name, String username) {
         this.name = name;
-        this.grade = grade;
+        this.username = username;
     }
 
     // returns the student's name
     public String getName() {
         return this.name;
-    };
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
     // adds the given grade to the grades property
     public void addGrade(int grade) {
         gradeList.add(grade);
-        System.out.println(gradeList);
-    };
+    }
+
     // returns the average of the students grades
     public double getGradeAverage() {
+        DecimalFormat numberFormat = new DecimalFormat("#.0");
+
         double sum = 0;
         for (int i = 0; i < gradeList.size(); i++) {
             sum += gradeList.get(i);
